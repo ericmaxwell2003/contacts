0318038d146b1d56f31afd614bbd6ca24734b933
ericmaxwell2003 <ericmaxwell2003@yahoo.com>
2019-07-28 10:40:29 -0400

    Lab 4 Explicit Deep Links part done

--- app/src/main/java/com/acme/contacts/detail/ContactDetailFragment.kt
+++ app/src/main/java/com/acme/contacts/detail/ContactDetailFragment.kt
@@ -2,18 +2,22 @@ package com.acme.contacts.detail
 
 import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.view.inputmethod.InputMethodManager
 import android.view.inputmethod.InputMethodManager.HIDE_NOT_ALWAYS
+import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavDeepLinkBuilder
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.onNavDestinationSelected
+import com.acme.contacts.FAVORITES_NOTIFICATION_CHANNEL_ID
 import com.acme.contacts.R
 import com.acme.contacts.databinding.FragmentContactDetailBinding
 
@@ -27,6 +31,9 @@ class ContactDetailFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(args.contactId != null) {
+            setHasOptionsMenu(true)
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -49,6 +56,22 @@ class ContactDetailFragment : Fragment() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.contact_details_option_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController()) ||
+                when(item.itemId) {
+                    R.id.simulate_notification -> {
+                        simulateNotification()
+                        true
+                    }
+                    else ->  super.onOptionsItemSelected(item)
+                }
+    }
+
     override fun onStop() {
         super.onStop()
 
@@ -57,6 +80,28 @@ class ContactDetailFragment : Fragment() {
                 .getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
             inputManager.hideSoftInputFromWindow(it.windowToken, HIDE_NOT_ALWAYS)
         }
+    }
+
+    fun simulateNotification() {
+        Toast.makeText(context, "Simulate Deep Link", Toast.LENGTH_SHORT).show()
+
+        val pendingIntent = findNavController().createDeepLink()
+            .setDestination(R.id.contact_detail)
+            .setArguments(arguments)
+            .createPendingIntent()
+
+        val notification = NotificationCompat
+            .Builder(requireContext(), FAVORITES_NOTIFICATION_CHANNEL_ID)
+            .setContentTitle(getString(R.string.encouragement_message_title))
+            .setSmallIcon(R.drawable.ic_contact_favorite_selected)
+            .setContentText(getString(R.string.encouragement_message_description,
+                                contactDetailVm.contact.value?.name ?: ""))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(requireContext())
+        notificationManager.notify(0, notification)
     }
 
     fun onSaveContact() {
